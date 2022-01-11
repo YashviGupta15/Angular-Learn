f1800b75521fb96a8e649f74b81e2bbfff2e859d
YashviGupta15 <yashvigupta12@gmail.com>
2022-01-11 10:37:13 +0530

    3 files modified; @Input() parent to child

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `
+    <h1>Hello {{name}}!</h1>
+    <p> {{message}} </p>
+    <p> {{num}} </p>
+  `,
+  styles: [`h1 { font-family: Lato; }`],
+  inputs: ['name', 'message'],
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class HelloComponent {
+  name: string;
+  message: string;
+  @Input('digit') num: number;
 }
